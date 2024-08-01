fde7b7f0ce7b8993071ce52d55b1b588d6917f92
Pankaj-mbm <pankajmbm1212@gmail.com>
2024-08-01 16:29:29 +0530

    new edit pankaj

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Job Educations',
+      title: 'Job Provider',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: AppColors.bgColor,
         textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
